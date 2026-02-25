152a4a592a16db1f4e58bced4355f84974c6cf38
chyroc <chyroc@qq.com>
2026-02-25 21:55:18 +0800

    fix(pygen): generate grouped root __init__ exports dynamically

--- internal/generator/python/generator.go
+++ internal/generator/python/generator.go
@@ -166,6 +166,13 @@ func writePythonSDK(
 			return err
 		}
 	}
+	rootInitContent, err := renderPythonRootInit(cfg)
+	if err != nil {
+		return err
+	}
+	if err := writer.write(filepath.Join(rootDir, "__init__.py"), rootInitContent); err != nil {
+		return err
+	}
 	if err := writePythonSpecialAssets(rootDir, writer); err != nil {
 		return err
 	}
@@ -180,7 +187,6 @@ func writePythonSpecialAssets(rootDir string, writer *fileWriter) error {
 		relPath string
 		asset   string
 	}{
-		{relPath: "__init__.py", asset: "special/cozepy/__init__.py.tpl"},
 		{relPath: "auth/__init__.py", asset: "special/cozepy/auth/__init__.py.tpl"},
 		{relPath: "websockets/__init__.py", asset: "special/cozepy/websockets/__init__.py.tpl"},
 		{relPath: "websockets/audio/__init__.py", asset: "special/cozepy/websockets/audio/__init__.py.tpl"},
